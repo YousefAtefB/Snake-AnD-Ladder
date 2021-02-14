1608b7c94efa3742884b2d3d3a6c7beadddab395
YousefAtefB <72487484+YousefAtefB@users.noreply.github.com>
2021-02-14 15:06:47 +0200

    the final code

---
+++ code/CardEight.cpp
@@ -0,0 +1,29 @@
+#include "CardEight.h"
+
+CardEight::CardEight(const CellPosition& pos) : Card(pos) 
+{
+	cardNumber = 8; 
+}
+
+CardEight::~CardEight(void)
+{
+}
+
+void CardEight::Apply(Grid* pGrid, Player* pPlayer)
+{
+	Card::Apply(pGrid, pPlayer);
+	Output* pOut = pGrid->GetOutput();
+	Input* pIn = pGrid->GetInput();
+	pOut->PrintMessage("You are Prevented From Rolling next time,Click any where to continue ...");
+	pIn->GetCellClicked();
+	pOut->ClearStatusBar();
+	pPlayer->PreventMoving(1);
+}
+
+void CardEight::Save(ofstream& Outfile, int Type)
+{
+	Card::Save(Outfile, Type);
+	if (Type == 2)
+	Outfile << endl;
+}
+
---
+++ code/CardEight.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Card.h"
+#include "RollDiceAction.h"
+
+#include "Grid.h"
+#include "Player.h"
+
+#include <time.h> 
+
+class CardEight : public Card  
+{
+public:
+	CardEight(const CellPosition& pos); // A Constructor takes card position
+
+	virtual void Apply(Grid* pGrid, Player* pPlayer); // Applies the effect of CardEight on the passed Player
+
+	void Save(ofstream& Outfile, int Type);
+
+	virtual ~CardEight(); // A Virtual Destructor
+};
+
